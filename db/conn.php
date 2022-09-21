6b8942c1a5955d710f47187b6acf534090ab1200
Redalebiad1 <Lebiad.contact@gmail.com>
2022-09-21 03:41:15 +0100

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,17 @@
 <?php
+    /* Developement Connection
     $host ='127.0.0.1';
     $db ='attendance_db';
     $user ='root';
     $pass ='';
+    $charset ='utf8mb4'; 
+    */
+
+   // Remote Database Connection
+    $host ='remotemysql.com';
+    $db ='aquut4S6pF';
+    $user ='aquut4S6pF';
+    $pass ='9yoAzdana6';
     $charset ='utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
